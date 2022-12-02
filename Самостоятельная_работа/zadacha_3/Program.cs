8913d0899a625a1fa0676df11c0f0649b245ba1d
Ekaterina6867 <Katya.judo@mail.ru>
2022-12-02 17:01:56 +0300

    решили третью задачу

---
+++ Самостоятельная_работа/zadacha_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+
+Console.WriteLine("введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", a, b);
+}
+else
+{
+ Console.WriteLine("{0} - максимальное значение, {1} - минимальное значение", b, a);
+}
