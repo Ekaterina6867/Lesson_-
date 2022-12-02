d3012663bb577bd31d4a6ff24e2a87bdc0396eaf
Ekaterina6867 <Katya.judo@mail.ru>
2022-12-02 16:25:28 +0300

    решиши первую задачу

---
+++ Самостоятельная_работа/zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+Console.WriteLine("введите два числа");
+//Var-это неявный тип, который означает, что система сама определит тип данных.
+//Split () - это строковый класс метод. Метод Split () возвращает массив строк, сгенерированных разбиением исходной строки,
+// разделенной разделителями, переданными в качестве параметра в методе Split ()
+
+//Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray(); считывает из консоли строку, 
+//разделяет её по пробелам, для каждой подстроки вызывает int.Parse и приводит полученную коллекцию к типу массива.
+var integers = Console.ReadLine().Split().Select(s => int.Parse(s)).ToArray();
+int first = integers[0];
+int second = integers[1];
+if (first==second*second)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("fals");
+}
