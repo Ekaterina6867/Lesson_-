8a68c4e1d388127c82c974d04001f5e597a6804a
Ekaterina6867 <Katya.judo@mail.ru>
2022-12-05 17:36:50 +0300

    решили вторую задачу

---
+++ домашние_задания/dz2/zad2/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("введите число");
+int n = int.Parse(Console.ReadLine());
+int k = (int)Math.Log10(n)-2;
+if (n>100)
+{
+Console.WriteLine(k < 0 ? "No such digit" : (n % (int)Math.Pow(10, k + 1) / (int)Math.Pow(10, k)).ToString());
+Console.ReadLine(); 
+}
+else 
+{
+Console.WriteLine(" число должно быть больше 99");
+}  
